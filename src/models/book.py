86404eedac865a6c7e3f4306f559afe95cd95337
Clau <clau.rjimenez@gmail.com>
2021-12-10 17:03:50 +0000

    added book model

---
+++ src/models/book.py
@@ -0,0 +1,34 @@
+from models.author import Author
+from models.publisher import Publisher
+
+class Book:
+    def __init__(
+        self,
+        ISBN: int,
+        title: str,
+        genre: str,
+        author: Author,
+        illustrator: Author,
+        publisher: Publisher,
+        edition: int,
+        cost: float,
+        price: float,
+        stock: int,
+        id: int = None
+    ):
+    
+        self.ISBN = ISBN
+        self.title = title
+        self.genre = genre
+        self.author = author
+        self.illustrator = illustrator
+        self.publisher = publisher
+        self.edition = edition
+        self.cost = cost
+        self.price = price
+        self.stock = stock
+        self.id = id
+        
+    def get_mark_up(self):
+        return round(self.price - self.stock, 2)
+    

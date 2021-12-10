b37c29de72a3c63de4f142c74cc220b96a1c6411
Clau <clau.rjimenez@gmail.com>
2021-12-10 16:50:20 +0000

    added publisher model

---
+++ src/models/publisher.py
@@ -0,0 +1,14 @@
+class Publisher:
+    def __init__(
+        self,
+        name: str,
+        website: str,
+        salesperson: str = 'No salesperson, order via website',
+        contact: str = None,
+        id: int = None
+    ):
+        self.name = name
+        self.website = website
+        self.salesperson = salesperson
+        self.contact = contact
+        self.id = id

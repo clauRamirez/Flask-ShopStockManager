17a5130d7ed731f2dc177b7222a5e9f6cc182972
Clau <clau.rjimenez@gmail.com>
2021-12-11 19:23:24 +0000

    finished adding functions

--- src/repositories/author_repository.py
+++ src/repositories/author_repository.py
@@ -3,9 +3,50 @@ from models.author import Author
 from typing import List
 
 
+def save(author: Author) -> None:
+    results = run_sql(
+        sql="\
+            INSERT INTO authors(name) \
+            VALUES (%s) RETURNING *;",
+        values=[author.name]
+    )
+    author.id = results[0]['id']
+
+
+def select(id: int) -> Author:
+    results = run_sql(
+        sql="SELECT * FROM authors WHERE id = %s;",
+        values=[id]
+    )
+    if results[0] is not None:
+        return Author(
+            name=results[0]['name'],
+            id=id
+        )
+
+
+def delete(id: int) -> None:
+    run_sql(
+        sql="DELETE FROM authors WHERE id = %s;",
+        values=[id]
+    )
+
+
+def update(author: Author) -> None:
+    run_sql(
+        sql="\
+            UPDATE authors SET(name) = (%s) \
+            WHERE id = %s;",
+        values=[
+            author.name,
+            author.id
+        ]
+    )
+
+
 def delete_all() -> None:
     run_sql(
-        sql="DELETE FROM authors"
+        sql="DELETE FROM authors;"
     )
 
 
@@ -15,17 +56,6 @@ def select_all() -> List[Author]:
             name=row['name'],
             id=row['id']
         ) for row in run_sql(
-            sql="SELECT * FROM authors"
+            sql="SELECT * FROM authors;"
         )
     ]
-
-
-def save(author: Author) -> None:
-    results = run_sql(
-        sql="\
-            INSERT INTO authors(name) \
-            VALUES (%s) RETURNING *;",
-        values=[author.name]
-    )
-    
-    author.id = results[0]['id']

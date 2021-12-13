745120fdea3dcee9fec493862bc7fbb532fa62e0
Clau <clau.rjimenez@gmail.com>
2021-12-13 19:50:39 +0000

    books controller and templates up and running

--- src/controllers/books_controller.py
+++ src/controllers/books_controller.py
@@ -1,9 +1,99 @@
 from flask import render_template, redirect, request
 from flask import Blueprint
-from repositories import book_repository
+from models.book import Book
+from repositories import book_repository, author_repository, publisher_repository
 
 books_blueprint = Blueprint("books", __name__)
 
-@books_blueprint.route("/books", methods=['GET'])
-def index():
-    return render_template("/books/index.html")
+# Title is not appearing in subdirectories i.e: /books/new etc
+_title = "Authors"
+
+@books_blueprint.route("/books", methods=['GET', 'POST'])
+def books_index():
+    if request.method == 'GET':
+        return render_template(
+            "/books/index.html",
+            title=_title,
+            books=book_repository.select_all()
+        )
+    if request.method == 'POST':
+        rf = request.form
+        
+        book_repository.save(
+            Book(
+                isbn=rf['isbn'],
+                title=rf['title'],
+                genre=rf['genre'],
+                author=author_repository.select(rf['author_id']),
+                illustrator=author_repository.select(rf['illustrator_id']),
+                publisher=publisher_repository.select(rf['publisher_id']),
+                edition=rf['edition'],
+                cost=rf['cost'],
+                price=rf['price'],
+                stock=rf['stock']
+            )
+        )
+        return redirect("/books")        
+
+
+@books_blueprint.route("/books/new", methods=['GET'])
+def books_new():
+    authors=author_repository.select_all()
+    
+    return render_template(
+        "/books/new.html",
+        authors=authors,
+        illustrators=authors,
+        publishers=publisher_repository.select_all()
+    )
+
+
+# SHOW   -> GET '/books/<id>'
+# UPDATE -> POST(PUT) '/books/<id>'
+@books_blueprint.route("/books/<int:id>", methods=['GET', 'POST'])
+def books_id(id):
+    if request.method == 'GET':
+        return render_template(
+            "/books/show.html",
+            book=book_repository.select(id)
+        )
+    if request.method == 'POST':
+        rf = request.form
+
+        book_repository.update(
+            Book(
+                isbn=rf['isbn'],
+                title=rf['title'],
+                genre=rf['genre'],
+                author=author_repository.select(rf['author_id']),
+                illustrator=author_repository.select(rf['illustrator_id']),
+                publisher=publisher_repository.select(rf['publisher_id']),
+                edition=rf['edition'],
+                cost=rf['cost'],
+                price=rf['price'],
+                stock=rf['stock'],
+                id=id
+            )
+        )
+        return redirect("/books")
+
+
+# EDIT -> GET '/books/<id>/edit'
+@books_blueprint.route("/books/<int:id>/edit", methods=['GET'])
+def books_edit(id):
+    authors = author_repository.select_all()
+    return render_template(
+        # this needs to change
+        "/books/edit.html",
+        book=book_repository.select(id),
+        authors=authors,
+        illustrators=authors,
+        publishers=publisher_repository.select_all()
+    )
+
+
+# DELETE -> POST(DELETE) '/books/<id>'
+@books_blueprint.route("/books/<id>/delete", methods=['POST'])
+def books_delete(id):
+    book_repository.delete(id)
+    return redirect('/books')
--- src/repositories/book_repository.py
+++ src/repositories/book_repository.py
@@ -28,39 +28,52 @@ def save(book: Book) -> None:
     book.id = results[0]['id']
 
 
-def select(id: int) -> Publisher:
+def select(id: int) -> Book:
     results = run_sql(
-        sql="SELECT * FROM publishers WHERE id = %s;",
+        sql="SELECT * FROM books WHERE id = %s;",
         values=[id]
     )
     if results[0] is not None:
-        return Publisher(
-            name=results[0]['name'],
-            website=results[0]['website'],
-            salesperson=results[0]['salesperson'],
-            contact=results[0]['contact'],
-            id=id
+        return Book(
+            isbn=results[0]['isbn'],
+            title=results[0]['title'],
+            genre=results[0]['genre'],
+            author=author_repository.select(results[0]['author_id']),
+            illustrator=author_repository.select(results[0]['illustrator_id']),
+            publisher=publisher_repository.select(results[0]['publisher_id']),
+            edition=results[0]['edition'],
+            cost=results[0]['cost'],
+            price=results[0]['price'],
+            stock=results[0]['stock'],
+            id=results[0]['id']
         )
 
 
 def delete(id: int) -> None:
     run_sql(
-        sql="DELETE FROM publishers WHERE id = %s;",
+        sql="DELETE FROM books WHERE id = %s;",
         values=[id]
     )
 
 
-def update(publisher: Publisher) -> None:
+def update(book: Book) -> None:
     run_sql(
         sql="\
-            UPDATE publishers SET(name, website, salesperson, contact) = (%s, %s, %s, %s) \
+            UPDATE books SET(isbn, title, genre, author_id, illustrator_id, publisher_id, edition, cost, price, stock) \
+            = (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) \
             WHERE id = %s;",
         values=[
-            publisher.name,
-            publisher.website,
-            publisher.salesperson,
-            publisher.contact,
-            publisher.id
+            book.isbn,
+            book.title,
+            book.genre,
+            book.author.id,
+            book.illustrator.id,
+            book.publisher.id,
+            book.edition,
+            book.cost,
+            book.price,
+            book.stock,
+            book.id
         ]
     )
 

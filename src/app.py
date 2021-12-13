592d692cc8ebb4822adf52cd4c72828e28d51e67
Clau <clau.rjimenez@gmail.com>
2021-12-13 23:02:27 +0000

    added .env and added enviromental variables where needed

--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv
+from os import getenv
 from flask import Flask
 from flask import render_template, request, redirect
+
+load_dotenv()
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -28,4 +32,4 @@ def not_found(error):
     return render_template('error.html'), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=getenv('DEBUG_MODE'))
--- src/db/run_sql.py
+++ src/db/run_sql.py
@@ -1,11 +1,15 @@
 import psycopg2
 import psycopg2.extras
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
 
 def run_sql(sql, values = None):
     results = []
     conn = None
     try:
-        conn = psycopg2.connect("dbname='shop_inventory'")
+        conn = psycopg2.connect(f"dbname={getenv('DATABASE_NAME')}")
         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cur.execute(sql, values)
         conn.commit()
